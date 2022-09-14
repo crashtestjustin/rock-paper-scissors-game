da15cd609ed5e2603bc7660529c77339247ba426
justin <thecrashtestjustin@gmail.com>
2022-09-13 22:10:18 -0700

    created a function to determine the outcome of a single gmae

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 const rpsOptions = ["Rock", "Paper", "Scissors"];
 
 
-function getComputerChoice (rpsOptions) {
+function getComputerChoice () {
     return rpsOptions[Math.floor(Math.random() * rpsOptions.length)];
 }
 
-console.log(getComputerChoice(rpsOptions));
+
+//take input from player selection
+//if player selection == cpu selection: announce tie
+//if player selection is rock and cpu selction paper: announce cpu wins
+//if player selection is rock and cpu selction scissors: announce player wins
+//if player selection is paper and cpu selction scissors: announce cpu wins
+//if player selection is paper and cpu selction rock: announce player wins
+//if player selection is scissors and cpu selction rock: announce cpu wins
+//if player selection is scissors and cpu selction paper: announce player wins
+//return message
+
+function playRound (playerSelection, computerSelection) {
+    computerSelection = getComputerChoice().toLowerCase();
+    playerSelection = playerChoice;
+    if (playerSelection == computerSelection) {
+        return "It's a tie!";
+    } else if ((playerSelection == "rock" && computerSelection == "paper") ||
+               (playerSelection == "paper" && computerSelection == "scissors") ||
+               (playerSelection == "scissors" && computerSelection == "rock")  
+    ){
+        return "You lose! " + computerSelection + " beats " + playerSelection + "!";
+    } else {
+        return "You won! " + playerSelection + " beats " + computerSelection + "!";
+    }
+}
+
+const playerChoice = "rock";
+
+console.log(playRound());
