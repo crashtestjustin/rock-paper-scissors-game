c8c71199982e86e28a2f2f0f249ef027791aa56a
justin <thecrashtestjustin@gmail.com>
2022-09-14 21:04:12 -0700

    created game function to read game status and score

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
 const rpsOptions = ["Rock", "Paper", "Scissors"];
+var cpuScore = 0;
+var playerScore = 0;
 
 
 function getComputerChoice () {
     return rpsOptions[Math.floor(Math.random() * rpsOptions.length)];
 }
 
-
-//take input from player selection
-//if player selection == cpu selection: announce tie
-//if player selection is rock and cpu selction paper: announce cpu wins
-//if player selection is rock and cpu selction scissors: announce player wins
-//if player selection is paper and cpu selction scissors: announce cpu wins
-//if player selection is paper and cpu selction rock: announce player wins
-//if player selection is scissors and cpu selction rock: announce cpu wins
-//if player selection is scissors and cpu selction paper: announce player wins
-//return message
-
 function playRound (playerSelection, computerSelection) {
-    computerSelection = getComputerChoice().toLowerCase();
-    playerSelection = playerChoice;
+        let playerChoice = prompt("Rock, Paper, or Scissors?");
+        computerSelection = getComputerChoice().toLowerCase();
+        playerSelection = playerChoice.toLowerCase();
     if (playerSelection == computerSelection) {
         return "It's a tie!";
-    } else if ((playerSelection == "rock" && computerSelection == "paper") ||
-               (playerSelection == "paper" && computerSelection == "scissors") ||
-               (playerSelection == "scissors" && computerSelection == "rock")  
+    } else if ((playerSelection === "rock" && computerSelection === "paper") ||
+               (playerSelection === "paper" && computerSelection === "scissors") ||
+               (playerSelection === "scissors" && computerSelection === "rock")  
     ){
-        return "You lose! " + computerSelection + " beats " + playerSelection + "!";
+        cpuScore++;
+        return loss = "You lose! " + computerSelection + " beats " + playerSelection + "!";
     } else {
-        return "You won! " + playerSelection + " beats " + computerSelection + "!";
+        playerScore++;
+        return win = "You won! " + playerSelection + " beats " + computerSelection + "!";
     }
 }
 
-const playerChoice = "rock";
+//create a function for tallying score
+//input the function in the for loop????
+
+function game(playerScore, cpuScore) {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+        if (playerScore > cpuScore) {
+                return "Congrats! You won the game " + playerScore + " to " + cpuScore + " !!"
+            } else if (cpuScore > playerScore) {
+                return "Sorry...You lost the game " + cpuScore + " to " + playerScore + " !!"
+        } else {
+            return "The game is still in progress. Who will win???"
+}
+}
+
 
 console.log(playRound());
+console.log(playerScore);
+console.log(cpuScore);
+console.log(game());
