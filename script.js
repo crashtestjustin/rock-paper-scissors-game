61fe799cb664030252d7b9040690ec6e4c53ef98
justin <thecrashtestjustin@gmail.com>
2022-09-17 14:11:03 -0700

    Updated game and score tally functions
    Split out the in game progess and the final score functions and
    nested them within the game loop to output an in progress message if
    within the 5 rounds or calculte a winner if not. Also updated the string
    outputs to better describe outcomes.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const rpsOptions = ["Rock", "Paper", "Scissors"];
 var cpuScore = 0;
 var playerScore = 0;
-let playerChose =  playerChoice();
+//let playerChose =  playerChoice();
 
 function getComputerChoice () {
     return rpsOptions[Math.floor(Math.random() * rpsOptions.length)];
@@ -12,36 +12,47 @@ function playerChoice() {
     return selection
 }
 
-function playRound (playerSelection, computerSelection) { 
+function playRound () { 
         computerSelection = getComputerChoice().toLowerCase();
         playerSelection = playerChoice().toLowerCase();
     if (playerSelection == computerSelection) {
-        return "It's a tie!"; 
+        return "This round is a tie!"; 
     } else if ((playerSelection === "rock" && computerSelection === "paper") ||
                (playerSelection === "paper" && computerSelection === "scissors") ||
                (playerSelection === "scissors" && computerSelection === "rock")  
     ){
         cpuScore++;
-        return loss = "You lose! " + computerSelection + " beats " + playerSelection + "!";
+        return loss = "You lose this round! " + computerSelection + " beats " + playerSelection + "!";
     } else {
         playerScore++;
-        return win = "You won! " + playerSelection + " beats " + computerSelection + "!";
+        return win = "You won this round! " + playerSelection + " beats " + computerSelection + "!";
     }
 }
 
-for (let i = 0; i < 5; ++i) {
+for (let i = 0; i < 5; i++) {
 function game() {
-        playRound();
-        if ( i === 5 && playerScore > cpuScore) {
-                return "Congrats! You won the game " + playerScore + " to " + cpuScore + " !!"
-            } else if ( i === 5 && cpuScore > playerScore) {
-                return "Sorry...You lost the game " + cpuScore + " to " + playerScore + " !!"
-        } else {
-            return "The game is still in progress. Who will win???"
-        }   
+        playRound();  
 }
-    console.log(playRound());
-    console.log(cpuScore);
-    console.log(playerScore);
-    console.log(game());
+function gameProgress() {
+    if (i < 4) {
+        return "The game is still in progress. Who will win???"
+    } else {
+        return finalScore();
+    }
+} 
+        console.log(playRound());
+        console.log("Computer Score: " + cpuScore);
+        console.log("Your Score: " + playerScore);    
+        console.log(gameProgress());
+}
+
+
+function finalScore() {
+    if (playerScore === cpuScore) {
+    return "The game has resulted in a tie. Would you like to play again?";
+} else if (playerScore > cpuScore) {
+    return "Congrats! You won the game " + playerScore + " to " + cpuScore + "!!";
+} else {
+    return "Sorry...You lost the game " + cpuScore + " to " + playerScore + "!!";
+}
 }
