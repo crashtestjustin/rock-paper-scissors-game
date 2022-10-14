9127841b916e62fca725b4134e1c164d497f2def
justin <thecrashtestjustin@gmail.com>
2022-10-14 07:28:11 -0700

    added detailed comments and the ability to count rounds and tally a 5 round winner

--- script.js
+++ script.js
@@ -1,46 +1,67 @@
+//declaring variables
 const rpsOptions = ["Rock", "Paper", "Scissors"];
 var cpuScore = 0;
 var playerScore = 0;
+var roundCounter = 1;
 let pPick;
 let computerPicks;
 let roundResult;
+let progress;
 
+//selectors
 const playerPicks = document.querySelectorAll('.rps-option');
 const playerScoreTally = document.querySelector('#player-score');
 const cpuScoreTally = document.querySelector('#computer-score');
+const pChoiceText = document.querySelector('#player-round-choice');
+const cpuChoiceText = document.querySelector('#computer-round-choice');
 
-const displayPlayerChoice = document.querySelector('#result');
-const scoresListed = document.querySelector('.scores');
-const playerChoice = document.createElement('div');
-playerChoice.classList.add('display-results');
-displayPlayerChoice.insertBefore(playerChoice, scoresListed);
+//creating a div within the #results-details div to hold the round results and 5 round game progress
+const displayPlayerChoice = document.querySelector('#result-details');
+const playerChoices = document.createElement('div');
+playerChoices.classList.add('display-results');
+displayPlayerChoice.append(playerChoices);
 
+//creating p elements within the div created above to house the round resuls and overall game progress
 const showRoundResults = document.querySelector('.display-results');
-const pChoiceText = document.createElement('p');
-const cpuChoiceText = document.createElement('p');
 const roundResultText = document.createElement('p');
-showRoundResults.append(pChoiceText, cpuChoiceText, roundResultText);
+const gameProgress = document.createElement('p');
+showRoundResults.append(roundResultText, gameProgress);
 
-
+//generating a random output from the array declared above
 function getComputerChoice () {
     return rpsOptions[Math.floor(Math.random() * rpsOptions.length)];
 }
 
-
-playerPicks.forEach((pick) => {
-    pick.addEventListener('click', e => {
-        pPick = pick.id;
-        computerPicks = getComputerChoice().toLowerCase();
-        playRound();
-        playerScoreTally.innerHTML = playerScore;
-        cpuScoreTally.innerHTML = cpuScore;
-        pChoiceText.textContent = "Player Chose: " + pPick;
-        cpuChoiceText.textContent = "Computer Chose: " + computerPicks;
-        roundResultText.textContent = "Result: " + roundResult;
+//Event listener on all .rps-option buttons as declared above - when clicked sets global variables pPick, computerPicks
+//to selections and runs playRound to compare and determine winner
+//Updates the innerHTML elements with scores and choices, rune trackProgress, and reveals the round result and game progress
+    playerPicks.forEach((pick) => {
+        pick.addEventListener('click', e => {
+            pPick = pick.id;
+            computerPicks = getComputerChoice().toLowerCase();
+            playRound();
+            playerScoreTally.innerHTML = playerScore;
+            cpuScoreTally.innerHTML = cpuScore;
+            pChoiceText.innerHTML = pPick;
+            cpuChoiceText.innerHTML = computerPicks;
+            roundResultText.textContent = "Result: " + roundResult;
+            roundCounter++;
+            trackProgress();
+            gameProgress.textContent = progress;
+        });
     });
-});
 
+//run on each click - looks to see if te round counter is still within 5 rounds - if true the progress global variable is
+//  = string - else the progress global varable = finalScore
+function trackProgress() {
+    if (roundCounter < 6) {
+        progress = "The game is still in progress. Who will win???"
+    } else {
+        progress = finalScore();
+    }
+}
 
+//compares the pPick and computerPicks global variables to determine who wins the round. Updates the roundResult global variable
 function playRound () { 
     if (pPick == computerPicks) {
         roundResult = "This round is a tie!"; 
@@ -56,33 +77,18 @@ function playRound () {
     }
 }
 
+//returns the final gmae outcome after 5 rounds have been played which is set to the global progress variable in the 
+//  trackProgress function
+function finalScore() {
+    if (playerScore === cpuScore) {
+    return "The game has resulted in a tie. Would you like to play again?";
+} else if (playerScore > cpuScore) {
+    return "Congrats! You won the game " + playerScore + " to " + cpuScore + "!!";
+} else {
+    return "Sorry...You lost the game " + cpuScore + " to " + playerScore + ".";
+}
+}
 
 
 
-
-
-//for (let i = 0; i < 5; i++) {
-// function playCompleteGame() {
-//         playRound();  
-// }
-// function gameProgress() {
-//     if (i < 4) {
-//         return "The game is still in progress. Who will win???"
-//     } else {
-//         return finalScore();
-//     }
-// } 
-        
-//}
-
-
-// function finalScore() {
-//     if (playerScore === cpuScore) {
-//     return "The game has resulted in a tie. Would you like to play again?";
-// } else if (playerScore > cpuScore) {
-//     return "Congrats! You won the game " + playerScore + " to " + cpuScore + "!!";
-// } else {
-//     return "Sorry...You lost the game " + cpuScore + " to " + playerScore + ".";
-// }
-// }
 
